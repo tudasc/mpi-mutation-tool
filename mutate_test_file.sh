5f9efb3fba01e5f52e007ff4c1f927f906c7f8b6
Tim Jammer <tim.jammer@tu-darmstadt.de>
2025-02-17 15:04:38 +0100

    more portable mutate_test_file.sh

--- mutate_test_file.sh
+++ mutate_test_file.sh
@@ -7,6 +7,5 @@ shift
 
 build/MPIMutationTool -extra-arg="-I/usr/lib/x86_64-linux-gnu/openmpi/include" -extra-arg="-I/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi" -extra-arg="-L/usr/lib/x86_64-linux-gnu/openmpi/lib" -extra-arg="-lmpi" $@ test_file.c
 
-clang-format-15 -i test_file.c
-
-vimdiff "$FILE" test_file.c
+clang-format -i test_file.c
+diff "$FILE" test_file.c
